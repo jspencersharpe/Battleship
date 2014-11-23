c19c2dea0cb56da957908966ba3f175801077dc5
Spencer Sharpe <jspencersharpe@gmail.com>
2014-11-23 15:50:12 -0600

    attempted rotation, wip

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 $(function() { 
-    $( "#ship" ).draggable({ grid: [ 40, 40 ] });
-    $( "#ship" ).draggable({ containment: "#my-ships", scroll: false });
-    
+    $( ".ship" ).draggable({ grid: [ 40, 40 ] });
+    $( ".ship" ).draggable({ containment: "#my-ships", scroll: false });
+});
+
+$('div#aircraft-carrier').toggle(
+function(){
+    $( this ).addClass( "carrier-sideways" );
+},
+function(){
+    $( this ).removeClass( "carrier-sideways" );
 });
 
 
