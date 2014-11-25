074be7e194456e449e7c98ea1a90d244cd28ba00
Spencer Sharpe <jspencersharpe@gmail.com>
2014-11-25 15:09:09 -0600

    added enemy board and fire button

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function() { 
     $( ".ship" ).draggable({grid: [ 43, 43 ]});
-    $( ".ship" ).draggable({ containment: ".Game-Board", scroll: false });
+    $( ".ship" ).draggable({ containment: "#my-ships", scroll: false });
     $( ".ship" ).draggable({snap: "td tr", revert: "valid"});
     $( ".ship" ).droppable({accept: ".ship", tolerance: "touch"});
 });
@@ -11,9 +11,11 @@ $ship.toggleClass('flip');
 event.preventDefault()
 });
 
-/*
-$('#aircraft-carrier').on('click', function(e) {
-    $( '.aircraft-carrier' ).toggleClass( "carrier-flip" );
-    //e.preventDefault()
-});
-*/
+//$('.targets').click(function() {
+  //td highlights yellow
+//});
+//$('#fire').click(function(){
+  //td turns red and sends message to firebase
+//});
+
+
