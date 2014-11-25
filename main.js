6681f025db7bb9b69e9e6c45d7cbf4243a2ae488
Spencer Sharpe <jspencersharpe@gmail.com>
2014-11-25 11:20:48 -0600

    ships rotating

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 $(function() { 
-    $( ".ship" ).draggable({grid: [ 40, 40 ]});
+    $( ".ship" ).draggable({grid: [ 43, 43 ]});
     $( ".ship" ).draggable({ containment: ".Game-Board", scroll: false });
     $( ".ship" ).draggable({snap: "td tr", revert: "valid"});
     $( ".ship" ).droppable({accept: ".ship", tolerance: "touch"});
+});
+
+$('.ship').dblclick(function(){
+var $ship = $(event.target);
+$ship.toggleClass('flip');
+event.preventDefault()
 });
 
 /*
