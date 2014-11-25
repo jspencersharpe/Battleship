a963a9facc8dd414d335d94175ed1b0225c0c244
Spencer Sharpe <jspencersharpe@gmail.com>
2014-11-25 10:22:56 -0600

    ships reverting

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 $(function() { 
     $( ".ship" ).draggable({grid: [ 40, 40 ]});
     $( ".ship" ).draggable({ containment: ".Game-Board", scroll: false });
-    $( ".ship" ).draggable({snap: "td tr" });
+    $( ".ship" ).draggable({snap: "td tr", revert: "valid"});
+    $( ".ship" ).droppable({accept: ".ship", tolerance: "touch"});
 });
 
 /*
-$('.aircraft-carrier').on('click', function(e) {
+$('#aircraft-carrier').on('click', function(e) {
     $( '.aircraft-carrier' ).toggleClass( "carrier-flip" );
     //e.preventDefault()
 });
