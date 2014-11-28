586d9dff5b0fec067ed01e0b4137dbca4e479d57
Spencer Sharpe <jspencersharpe@gmail.com>
2014-11-28 12:14:40 -0600

    worked on location tracking

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+var $spaces = $('#my-ships td')
+var $ship = $('.ship')
+
 $(function() { 
-    $( ".ship" ).draggable({grid: [ 43, 43 ]});
-    $( ".ship" ).draggable({ containment: "#my-ships", scroll: false });
-    $( ".ship" ).draggable({snap: "td tr", revert: "valid"});
-    $( ".ship" ).droppable({accept: ".ship", tolerance: "touch"});
-});
+    $ship.draggable({grid: [ 44, 44 ]});
+    $ship.draggable({ containment: "#my-ships", scroll: false });
+    $ship.draggable({snap: "td tr", revert: "valid"});
+    $ship.droppable({accept: $ship, tolerance: "touch"});
+    $("th").droppable({addClasses: false});
+    });
 
 //Rotates ships
 $('.ship').dblclick(function(){
@@ -18,14 +22,14 @@ $('.targets').click(function(e) {
   $('.targets').removeClass('highlighted');
   $(this).addClass('highlighted');
   e.stopPropagation();
-      return true;
+      return $choice;
 });
 
 $('#fire').click(function(e){
   var $highLight = $('td').hasClass('highlighted');
   if ($highLight === true) {
   $('td').removeClass('highlighted');
-  $('td').focus().css("background-color", "red");
+  $('.target:selected').addClass('red');
   console.log("works");
   e.stopPropagation();
   }
